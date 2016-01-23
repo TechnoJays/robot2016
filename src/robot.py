59bf70ce8c7e9e66ad5788e2c500e1a838bae9f2
Ty Strayer <tstrayer@ford.com>
2016-01-23 02:15:11 -0500

    Initial commit for the TechnoJays 2016 robot code.
    Robot code will use the command pattern.
    
    Initial project setup using the command robot template from the RobotPy Eclipse plugin.
    
    Added __init__.py files to package directories.
    
    Added directory for config ini files for use with ConfigParser.
    
    Corrected imports in template files.

---
+++ src/robot.py
@@ -0,0 +1,40 @@
+
+from wpilib import command
+import wpilib
+
+from commands.example_command import ExampleCommand
+from subsystems.example_subsystem import ExampleSubsystem
+
+
+class MyRobot(wpilib.IterativeRobot):
+
+    example_subsystem = ExampleSubsystem()
+    oi = None
+
+    def robotInit(self):
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        self.oi = None
+        #Create the command used for the autonomous period
+        self.autonomous_command = ExampleCommand(self)
+
+    def autonomousInit(self):
+        #Schedule the autonomous command
+        self.autonomous_command.start()
+
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
+        command.Scheduler.getInstance().run()
+
+    def teleopPeriodic(self):
+        """This function is called periodically during operator control."""
+        command.Scheduler.getInstance().run()
+
+    def testPeriodic(self):
+        """This function is called periodically during test mode."""
+        wpilib.LiveWindow.run()
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
