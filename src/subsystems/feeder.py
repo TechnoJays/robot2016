58e9a589cc285e9cd0d7f3a29334e2bbdb440a9d
Ty Strayer <tstrayer@ford.com>
2016-02-06 14:32:54 -0500

    Added class stubs for subsystems.

---
+++ src/subsystems/feeder.py
@@ -0,0 +1,16 @@
+'''
+Created on Feb 6, 2016
+
+@author: tylerstrayer
+'''
+from wpilib.command.subsystem import Subsystem
+
+class Feeder(Subsystem):
+    
+    def __init__(self, robot, name=None):
+        pass
+    
+    def initDefaultCommand(self):
+        return Subsystem.initDefaultCommand(self)
+
+    
---
+++ src/subsystems/scaling_arm.py
@@ -0,0 +1,15 @@
+'''
+Created on Feb 6, 2016
+
+@author: tylerstrayer
+'''
+from wpilib.command.subsystem import Subsystem
+
+class ScalingArm(Subsystem):
+    
+    def __init__(self, robot, name=None):
+        pass
+    
+    def initDefaultCommand(self):
+        return Subsystem.initDefaultCommand(self)
+    
