53acfd0154d636e20a78c665940fd9c19dcd9c68
Ty Strayer <tstrayer@ford.com>
2016-02-18 16:56:38 -0500

    Stubbed in methods needed for FeedIn command.

--- src/subsystems/feeder.py
+++ src/subsystems/feeder.py
@@ -5,6 +5,11 @@ Created on Feb 6, 2016
 '''
 from wpilib.command.subsystem import Subsystem
 
+class Direction(object):
+    """Enumerates feeder direction"""
+    IN = 0
+    OUT = 1
+
 class Feeder(Subsystem):
     
     def __init__(self, robot, name=None):
@@ -13,4 +18,9 @@ class Feeder(Subsystem):
     def initDefaultCommand(self):
         return Subsystem.initDefaultCommand(self)
 
+    def spinFeeder(self, direction):
+        pass
+    
+    def hasBall(self):
+        return False
     
