553d7d4f7b662f0ea4358d3e128bfaeef3977fc6
Aaron DeGrow <adeinpublic@gmail.com>
2016-03-04 14:16:44 -0500

    Fixes

--- src/commands/auto_mode.py
+++ src/commands/auto_mode.py
@@ -19,24 +19,24 @@ class AutoCommandGroup(CommandGroup):
     '''
     _robot = None
     _config = None
-    
+
     _drivetrain_section = "Drivetrain"
     _auto_speed = None
     _drivetrain_threshold = None
     _drivetrain_ramp_threshold = None
-    
+
     _feeder_section = "Feeder"
     _feeder_speed = None
     _feed_time = None
-    
+
     _arm_section = "Arm"
     _lower_bound = None
     _lower_speed = None
-    
+
     _hook_section = "Hook"
     _extend_speed = None
     _raise_stop_count = None
-    
+
     _starting_obstacle = None # the obstacle we're starting in front of in autonomous mode
     _target_obstacle = None # the obstacle we want to traverse during autonomous mode
     _return_obstacle = None # the obstacle we want to end on during autonomous mode
@@ -47,7 +47,7 @@ class AutoCommandGroup(CommandGroup):
     _shoot_point = None # point from which we want to shoot the ball, at foot of ramp
     _lane_width = None # distance to travel from the center of one obstacle to the center of another
     _cross_obstacle = None # distance to travel to traverse obstacle, unneeded if crossing based on pitch gyro
-    
+
     # command groups
     _approach_commands = CommandGroup()
     _cross_commands = CommandGroup()
@@ -60,7 +60,7 @@ class AutoCommandGroup(CommandGroup):
         self._config.read(configfile)
         self._init_commands()
         self.robot.drivetrain.reset_gyro_angle()
-        
+
         self.add_approach_commands()
         #self.add_cross_commands()
         #self.add_score_commands()
@@ -76,7 +76,7 @@ class AutoCommandGroup(CommandGroup):
         elif (self._target_obstacle < self._starting_obstacle):
             self._obstacle_direction = -1
         elif (self._target_obstacle == self._starting_obstacle):
-            self._obstacle_direction = 0        
+            self._obstacle_direction = 0
 
     def add_approach_commands(self):
         # lower arm
@@ -94,11 +94,11 @@ class AutoCommandGroup(CommandGroup):
             # turn to face obstacle
             right_angle = right_angle * -1
             self._approach_commands.addSequential(turn_degrees.TurnDegrees(self._robot, right_angle, self._auto_speed, self._drivetrain_threshold, self._drivetrain_ramp_threshold))
-        
+
         # drive to obstacle line
-        self._approach_commands.addSequential(drive_encoder_counts.DriveEncoderCounts(self._robot, self._distance_to_obstacle, self._auto_speed, self._drivetrain_threshold, self._drivetrain_ramp_threshold))   
+        self._approach_commands.addSequential(drive_encoder_counts.DriveEncoderCounts(self._robot, self._distance_to_obstacle, self._auto_speed, self._drivetrain_threshold, self._drivetrain_ramp_threshold))
         self.addSequential(self._approach_commands)
-            
+
     def add_cross_commands(self):
         # todo: use pitch gyro to determine if we've crossed, drive until true
         # cross the obstacle
@@ -106,17 +106,17 @@ class AutoCommandGroup(CommandGroup):
         # re-center
         self._cross_commands.addSequential(turn_degrees_absolute.TurnDegreesAbsolute(self._robot, 0, self._speed, self._drivetrain_threshold, self._drivetrain_ramp_threshold))
         self.addSequential(self._cross_commands)
-        
+
     def add_score_commands(self):
         # angle of hyp/adj
         angle = math.degrees(math.atan(self._target_point / self._lane_width))
         # length of hyp
         hypotenuse = math.sqrt((self._target_point ** 2) + (self._lane_width ** 2))
-        
+
         '''
         this if statement is ugly af. is there a better way to accomplish this?
         '''
-        
+
         if (self._target_obstacle == 1 or self._target_obstacle == 4):
             # turn to the right at [angle]
             self._score_commands.addSequential(turn_degrees.TurnDegrees(self._robot, angle, self._auto_speed, self._drivetrain_threshold, self._drivetrain_ramp_threshold))
@@ -150,15 +150,15 @@ class AutoCommandGroup(CommandGroup):
         # shoot
         self._score_commands.addSequential(feed_ball_out.FeedBallOut(self._robot, self._feeder_speed, self._feed_time))
         self.addSequential(self._score_commands)
-        
+
     def add_return_commands(self):
         # todo: drive to return obstacle
         # turn around
         # self._return_commands.addSequential(turn_degrees.TurnDegrees(self._robot, 180, self._auto_speed, self._drivetrain_threshold, self._drivetrain_ramp_threshold))
         # return to target point
         # self._return_commands.addSequential(drive_encoder_counts.DriveEncoderCounts(self._robot, self._shoot_point, self._auto_speed, self._drivetrain_threshold, self._drivetrain_ramp_threshold))
-        # 
-    
+        #
+
         if (self._target_obstacle == 1 or self._target_obstacle == 2 or self._target_obstacle == 3):
             # turn to face obstacles
             self._return_commands.addSequential(turn_degrees.TurnDegrees(self._robot, 135, self._auto_speed, self._drivetrain_threshold, self._drivetrain_ramp_threshold))
@@ -168,64 +168,26 @@ class AutoCommandGroup(CommandGroup):
         # drive to obstacles
         self._return_commands.addSequential(drive_encoder_counts.DriveEncoderCounts(self._robot, self._target_point, self._auto_speed, self._drivetrain_threshold, self._drivetrain_ramp_threshold))
         self.addSequential(self._return_commands)
-        
+
     def _init_commands(self):
         # drivetrain
         self._distance_to_obstacle = self._config.getint(self._drivetrain_section, "DISTANCE_TO_OBSTACLE")
-        self._auto_speed = self._config.getint(self._drivetrain_section, "AUTO_SPEED")
+        self._auto_speed = self._config.getfloat(self._drivetrain_section, "AUTO_SPEED")
         self._drivetrain_threshold = self._config.getint(self._drivetrain_section, "THRESHOLD")
         self._drivetrain_ramp_threshold = self._config.getint(self._drivetrain_section, "RAMP_THRESHOLD")
         self._cross_obstacle = self._config.getint(self._drivetrain_section, "CROSS_OBSTACLE")
         self._target_point = self._config.getint(self._drivetrain_section, "TARGET_POINT")
         self._lane_width = self._config.getint(self._drivetrain_section, "LANE_WIDTH")
         self._shoot_point = self._config.getint(self._drivetrain_section, "SHOOT_POINT")
-        
+
         # feeder
         self._feeder_speed = self._config.getfloat(self._feeder_section, "FEEDER_SPEED")
-        self._feed_time = self._config.getint(self._feeder_section, "FEED_TIME")
-        
+        self._feed_time = self._config.getfloat(self._feeder_section, "FEED_TIME")
+
         # arm
         self._lower_bound = self._config.getint(self._feeder_section, "LOWER_BOUND")
         self._lower_speed = self._config.getfloat(self._feeder_section, "LOWER_SPEED")
-        
+
         # hook
         self._extend_speed = self._config.getfloat(self._hook_section, "EXTEND_SPEED")
         self._raise_stop_count = self._config.getint(self._hook_section, "RAISE_STOP_COUNT")
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
