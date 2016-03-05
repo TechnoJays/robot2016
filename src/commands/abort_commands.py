4ac6b326aa1d67ff5798749d2553ff78fc39f9a3
Aaron DeGrow <adeinpublic@gmail.com>
2016-03-04 20:07:29 -0500

    Added abort command, but not used yet

---
+++ src/commands/abort_commands.py
@@ -0,0 +1,40 @@
+from wpilib.command.command import Command
+
+
+class Abort(Command):
+    _ran_once = False
+
+    def __init__(self, robot, name=None, timeout=None):
+        '''
+        Constructor
+        '''
+        super().__init__(name, timeout)
+        self.robot = robot
+        self.requires(robot.arm)
+        self.requires(robot.drivetrain)
+        self.requires(robot.feeder)
+        self.requires(robot.hook)
+
+    def initialize(self):
+        """Called before the Command is run for the first time."""
+        self._ran_once = False
+
+    def execute(self):
+        """Called repeatedly when this Command is scheduled to run"""
+        self.robot.drivetrain.arcade_drive(0,0)
+        self.robot.feeder.spinFeeder(0)
+        self.robot.hook.move_hook(0)
+        self.robot.arm.move_arm(0)
+        self._ran_once = True
+
+    def isFinished(self):
+        """Returns true when the Command no longer needs to be run"""
+        return self._ran_once or self.isTimedOut()
+
+    def end(self):
+        """Called once after isFinished returns true"""
+        pass
+
+    def interrupted(self):
+        """Called when another command which requires one or more of the same subsystems is scheduled to run"""
+        self.end()
